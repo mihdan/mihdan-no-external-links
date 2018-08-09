166c1991fb69de829f9e5fef4569cdd306449ab3
mihdan <mikhail@kobzarev.com>
2018-08-09 14:15:41 +0300

    Fix bug with `Mihdan_NoExternalLinks_Admin_Log_Table`

--- includes/mihdan-noexternallinks.php
+++ includes/mihdan-noexternallinks.php
@@ -538,10 +538,16 @@ class Mihdan_NoExternalLinks {
         $this->loader->add_action( 'admin_init', $this->admin, 'register_setting' );
 
         $this->loader->add_filter( 'set-screen-option', $this->admin, 'mask_page_set_screen_options', NULL, 3 );
-        $this->loader->add_action( 'load-toplevel_page_' . $this->plugin_name, $this->admin, 'mask_page_screen_options' );
+        $this->loader->add_action( 'load-toplevel_page_' . $this->get_plugin_name(), $this->admin, 'mask_page_screen_options' );
 
         $this->loader->add_filter( 'set-screen-option', $this->admin, 'log_page_set_screen_options', NULL, 3 );
-        $this->loader->add_action( 'load-external-links_page_' . $this->get_plugin_name() . '-logs', $this->admin, 'log_page_screen_options' );
+        //$this->loader->add_action( 'load-external-links_page_' . $this->get_plugin_name() . '-logs', $this->admin, 'log_page_screen_options' );
+
+		$hook_name = vsprintf( 'load-%s_page_%s-logs', array(
+			sanitize_title( __( 'External Links', $this->plugin_name ) ),
+			$this->get_plugin_name()
+		) );
+        $this->loader->add_action( $hook_name, $this->admin, 'log_page_screen_options' );
 
         $this->loader->add_action( 'add_meta_boxes', $this->admin, 'add_custom_meta_box' );
         $this->loader->add_action( 'save_post', $this->admin, 'save_custom_meta_box' );
