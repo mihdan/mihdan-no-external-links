f76a7629fad9b9a477e7fbd7b2f000d1c7a98ce2
petrozavodsky <petrozavodsky@gmail.com>
2019-04-12 11:35:02 +0300

    Условие проверяющие возникновение фатальной ошибки

--- admin/partials/logs.php
+++ admin/partials/logs.php
@@ -13,9 +13,11 @@
     <h1><?php echo esc_html( get_admin_page_title() ) ?></h1>
 
     <form method="post">
-        <?php
-        $this->logs_table->prepare_items();
-        $this->logs_table->display();
-        ?>
+		<?php
+		if ( $this->logs_table ) {
+			$this->logs_table->prepare_items();
+			$this->logs_table->display();
+		}
+		?>
     </form>
 </div>
