a10b25fa750e084265b230a4a940def59b750c09
Mikhail Kobzarev <mikhail@kobzarev.com>
2023-05-03 00:25:57 +0300

    Resolve #30

--- public/js/seo-hide.js
+++ public/js/seo-hide.js
@@ -4,20 +4,45 @@
             el.addEventListener(
                 'click',
                 ( e ) => {
-                    const link   = el.getAttribute( 'data-link' );
-                    const target = el.getAttribute( 'data-target' );
+                    let link   = el.getAttribute( 'data-link' );
+                    let target = el.getAttribute( 'data-target' );
 
                     if ( ! link ) {
                         return;
                     }
 
+					link = decodeHTMLEntities( atob ( link ) );
+
                     if ( target && '_blank' === target ) {
-                        w.open( atob( link ) );
+                        w.open( link );
                     } else {
-                        d.location.href = atob( link );
+                        d.location.href = link;
                     }
                 }
             );
         }
     );
+
+	function basePropertyOf( object ) {
+		return function ( key ) {
+			return object == null ? undefined : object[ key ];
+		};
+	}
+
+	var reEscapedHtml = /&(?:amp|lt|gt|quot|#39);/g,
+		reHasEscapedHtml = RegExp( reEscapedHtml.source ),
+		htmlUnescapes = {
+			'&amp;': '&',
+			'&lt;': '<',
+			'&gt;': '>',
+			'&quot;': '"',
+			'&#39;': "'"
+		},
+		unescapeHtmlChar = basePropertyOf( htmlUnescapes );
+
+	function decodeHTMLEntities( text ) {
+		return ( text && reHasEscapedHtml.test( text ) )
+			? text.replace( reEscapedHtml, unescapeHtmlChar )
+			: text;
+	}
 } )( window, document );
